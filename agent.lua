bc4b275c3b88a95f68376e7e7fda2f2079f21d39
Phil Pirj <pirj@mail.ru>
2010-02-22 14:36:06 +0300

    cleaning up libs

--- agent.lua
+++ agent.lua
@@ -1,27 +1,28 @@
 require 'util'
-require 'luarocks.require'
+require 'luarocks.require' -- 
 
-local http = require 'socket.http'
+local http = require 'socket.http' -- 
 
 local bot_container = 'http://localhost:9292/whatnow'
 local body, status = http.request(bot_container, '')
 
-local json = require('json')
+local json = require('json') -- 
 local dec = json.decode(body)
 
 -- local url = 'http://localhost:9292'
 local url = 'http://s5.travian.ru'
 local htmlf = http.request(url..dec[2])
 
-require 'html'
+local html = require 'lib/html' -- 
 local parsed_html = html.parsestr(htmlf)
 local xml = to_html(parsed_html[1])
 
-local lom = require 'lxp.lom'
+local lom = require 'lxp.lom' -- 
 local parsed = lom.parse(xml)
 
-local xpath = require 'xpath'
-local found = xpath.selectNodes(parsed, "//form//input[@name='login']")
+local xpath = require 'lib/xpath' -- http://luaxpath.luaforge.net/
+-- local found = xpath.selectNodes(parsed, "//form//input[@name='login']")
+local found = xpath.selectNodes(parsed, "//input")
 
 print(to_string(found))
 
