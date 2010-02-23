554b21c8cf85636d0fa93aaf9b5b8231a5d841af
Phil Pirj <pirj@mail.ru>
2010-02-23 18:19:20 +0300

    works fine: server interaction == demo1

--- agent.lua
+++ agent.lua
@@ -1,32 +1,31 @@
 require 'util'
-require 'luarocks.require' -- 
+require 'luarocks.require' -- http://www.luarocks.org/
 
-local http = require 'socket.http' -- 
+local http = require 'socket.http' -- http://www.tecgraf.puc-rio.br/~diego/professional/luasocket/
 
-local bot_container = 'http://localhost:9292/whatnow'
-local body, status = http.request(bot_container, '')
+local bot_container = 'http://dozorni.heroku.com/'
+local body, status = http.request(bot_container, 'whatnow')
 
-local json = require('json') -- 
+local json = require('json') -- http://luaforge.net/projects/luajson/
 local dec = json.decode(body)
 
--- local url = 'http://localhost:9292'
 local url = 'http://s5.travian.ru'
 local htmlf = http.request(url..dec[2])
 
-local html = require 'lib/html' -- 
+local html = require 'lib/html' -- http://luaforge.net/projects/html/
 local parsed_html = html.parsestr(htmlf)
 local xml = to_html(parsed_html[1])
 
-local lom = require 'lxp.lom' -- 
+local lom = require 'lxp.lom' -- http://www.keplerproject.org/luaexpat/
 local parsed = lom.parse(xml)
 
 local xpath = require 'lib/xpath' -- http://luaxpath.luaforge.net/
--- local found = xpath.selectNodes(parsed, "//form//input[@name='login']")
-local found = xpath.selectNodes(parsed, "//input")
+local found = xpath.selectNodes(parsed, "//form//input[@name='login']")
 
 print(to_string(found))
 
--- print('xxx')
--- print(#found)
--- print(found[1].tag)
--- print(found[1][1])
+print(#found)
+print(found[1].tag)
+print(found[1].attr.value)
+print(found[1].attr.name)
+print(found[1].attr.id)
