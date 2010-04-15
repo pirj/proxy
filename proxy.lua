bc30a0349c17e997dbd1c374c8febd588ef5b9f2
Phil Pirj <pirj@mail.ru>
2010-04-16 00:30:54 +0400

    coroutine based proxy

--- proxy.lua
+++ proxy.lua
@@ -6,24 +6,46 @@ local socket = require 'socket' -- same
 local HOME_URL = 'localhost'
 local PORT = '1080'
 
-local server = socket.bind(HOME_URL, PORT)
-
 require 'copas' -- http://keplerproject.github.com/copas/
 
-copas.addserver(server, echoHandler)
-
-function handler(sock)
-  sock = copas.wrap(sock)
-  while true do
-    local data = sock:receive()
-    if data == 'quit' then
-      break
+local function simple(host, port, handler)
+  print("starting at "..host..':'..port)
+  return copas.addserver(assert(socket.bind(host, port)),
+    function(c)
+      return handler(copas.wrap(c), c:getpeername())
     end
-    sock:send(data)
-  end
+  )
 end
 
-while true do
+local function daytime_handler(c, host, port)
+    print("daytime connection from", host, port)
+    c:send(os.date() .. '\r\n')
+end
+
+local function echo_handler(c, host, port)
+    print("echo connection from", host, port)
+    repeat
+        local line = c:receive"*l"
+        if line then c:send(line .. '\r\n') end
+    until not line
+    print("echo termination from", host, port)
+end
+
+local shutdown = false
+
+local function shutdown_handler(c, host, port)
+  print("shutting down", host, port)
+  shutdown = true
+end
+
+-- Use 0 to listen on the standard (privileged) ports.
+local offset = ... or 10000
+
+simple("*", offset + 7, echo_handler)
+simple("*", offset + 9, shutdown_handler)
+simple("*", offset + 13, daytime_handler)
+
+while not shutdown do
   copas.step()
 
 end
