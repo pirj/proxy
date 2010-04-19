9310b1c8f5a049a02384e4a590dabb7ac2f7dccc
Phil Pirj <pirj@mail.ru>
2010-04-19 22:08:24 +0400

    simplify

--- proxy.lua
+++ proxy.lua
@@ -3,65 +3,51 @@ require 'luarocks.require' -- http://www.luarocks.org/
 local http = require 'socket.http' -- http://www.tecgraf.puc-rio.br/~diego/professional/luasocket/
 local socket = require 'socket' -- same
 local ltn12 = require 'ltn12' -- http://www.tecgraf.puc-rio.br/~diego/professional/luasocket/ltn12.html
-require 'copas' -- http://keplerproject.github.com/copas/
+local copas = require 'copas' -- http://keplerproject.github.com/copas/
 
 local PORT = '1080'
 
-local function server(host, port, handler)
-  print('starting at '..host..':'..port)
-  return copas.addserver(assert(socket.bind(host, port)),
-    function(c)
-      return handler(copas.wrap(c), c:getpeername())
-    end
-  )
-end
-
 function socket_source(sock)
   return function()
-    return sock:receive('*l')
+    local line = sock:receive('*l')
+    if line == '' then return nil end
+    return line..'\r\n'
   end
 end
 
-local function handler(sock_in, host, port)
+local function handler(sock_in)
+  sock_in = copas.wrap(sock_in)
   local line = sock_in:receive('*l')
-  if line then
-    print(line)
-    if string.find(line, 'travian') then
-      print('travian')
 
-    else
-      print('non-travian, pulling transparently')
+  if not line or line == '' then
+    print('error: empty input')
+    return nil
+  end
 
-      -- local source = socket.source('until-closed', sock_in)
-      -- local source_compound = ltn12.source.cat(
-      --   ltn12.source.string(line..'\r\n'),
-      --   ltn12.source.string('Connection: close\r\n'),
-      --   socket_source(sock_in),
-      --   ltn12.source.string('\r\n'),
-      --   ltn12.source.string('\r\n')
-      -- ) -- ltn12.source.simplify(
-      -- 
+  -- where to?
+  local url = string.match(line, 'http://([%a%d\.-]+):*%d*/')
+  local port = string.match(line, 'http://[%a%d\.-]+:(%d+)/')
+  if not url then
+    print('error:unparsable url'..line)
+    return nil
+  end
+  local sock_out = socket.connect(url, port or 80)
+  -- sock_out = copas.wrap(sock_out)
 
-      -- local sink = socket.sink('close-when-done', sock_out)
-      -- 
-      -- ltn12.pump.all(source_compound, sink)
+  if string.find(line, 'travian') then
+    print('travian')
 
-      local url = string.match(line, 'http://([%a%d\.-]+):*%d*/')
-      local port = string.match(line, 'http://[%a%d\.-]+:(%d+)/')
-      if not url then print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: "..line) end
-      local sock_out = socket.connect(url, port or 80)
+  else
+    repeat
+      if line then sock_out:send(line..'\r\n') end
+      -- if line then sock_out:send(line) end
+      line = sock_in:receive('*l')
+    until not line or line == '\r\n' or line == '\n' or line == '\r' or line == ''
+    sock_out:send('Connection: close\r\n')
+    sock_out:send('\r\n')
 
-      repeat
-        if line then sock_out:send(line..'\r\n') end
-        line = sock_in:receive('*l')
-      until not line or line == '\r\n' or line == '\n' or line == '\r' or line == ''
-      sock_out:send('Connection: close\r\n')
-      sock_out:send('\r\n')
-
-      source = socket.source('until-closed', sock_out)
-      sink = socket.sink('close-when-done', sock_in)
-      ltn12.pump.all(source, sink)
-    end
+    local response = sock_out:receive('*a')
+    if line then sock_in:send(response) end
   end
 end
 
@@ -71,11 +57,12 @@ local function shutdown_handler(c, host, port)
   shutdown = true
 end
 
-server('localhost', PORT, handler) -- * for address makes it available from any host, not just localhost
-server('localhost', PORT + 1, shutdown_handler)
+copas.addserver(socket.bind('localhost', PORT + 1), shutdown_handler)
+copas.addserver(socket.bind('localhost', PORT), handler)
 
 -- copas.loop()
 while not shutdown do
   copas.step()
+  print('step')
   -- more logic in the loop
 end
