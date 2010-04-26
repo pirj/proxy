d5c04a16332fa3e18e47a3a8e85079bd205e26ed
Phil Pirj <pirj@mail.ru>
2010-04-27 00:50:31 +0400

    trial and error: still getting receive timeouts

--- async.lua
+++ async.lua
@@ -7,49 +7,55 @@ local connections = {} -- sockets
 
 function connect(host, port, co)
   local sock = socket.tcp()
-  -- sock:settimeout(0)
-  local res, err = sock:connect(host, port)
+  sock:settimeout(0)
   table.insert(connections, sock)
   connections_coroutines[sock] = co
-  print('setting', sock, 'to', co)
-  if not res then
-    print('CONN ERR:', err)
+  local res, err = sock:connect(host, port)
+  if err == 'timeout' then
+    while not sock:getpeername() do
+      print('conn timeout, yield', host, port, sock:getpeername())
+      coroutine.yield()
+    end
+  elseif err then
+    print('async conn err:', err)
     return nil, err
   end
-  print('CONN out', sock, res)
+  print('CONN out', sock)
   return sock
 end
 
-function receive(url, sock, ...)
-  local data, err
-  while not data do
-    data, err = sock:receive(...)
-    if err == 'timeout' then
-      print('timeout', url)
+function receive(url, sock, pattern)
+  local data1, err1
+  while not data1 do
+    data1, err1 = sock:receive(pattern)
+    if err1 == 'timeout' then
+      -- print('receive timeout', url)
       coroutine.yield()
-    elseif err then
-      print('async receive err:', err)
-      return data, err
+    elseif err1 then
+      print('async receive err:', err1)
+      return nil, err1
     end
   end
 
-  return data
+  return data1
 end
 
-function send(url, sock, ...)
+function send(url, sock, data_to_send)
   local data, err
   while not data do
-    data, err = sock:send(...)
+    print('sending via sock', sock)
+    data, err = sock:send(data_to_send)
     if err == 'timeout' then
-      print('timeout', url)
+      print('send timeout', url)
       coroutine.yield()
     elseif err then
       print('async send err:', err)
-      return data, err
+      return nil, err
     end
   end
+  print('sent', data)
 
-  return data, err
+  return data
 end
 
 function server(port, handler)
@@ -67,6 +73,7 @@ function server(port, handler)
     for i, connection in ipairs(ins) do
       if not connections_coroutines[connection] then
         local client, err = connection:accept()
+        print('incoming')
         connections_coroutines[client] = coroutine.create(function(co)
           handler(client, co)
         end)
@@ -77,19 +84,29 @@ function server(port, handler)
     end
 
     for i, connection in ipairs(outs) do
-      print('outs', connection, connections_coroutines[connection])
+      -- print('outs', connection, connections_coroutines[connection])
       if connections_coroutines[connection] then
         cos_to_wake_up[connections_coroutines[connection]] = true
       end
     end
-    
+
     for co in pairs(cos_to_wake_up) do
+      print('resuming', co)
       coroutine.resume(co, co)
       if coroutine.status(co) == 'dead' then
         for i, connection in ipairs(connections) do
           if connections_coroutines[connection] == co then
             connections_coroutines[connection] = nil
-            table.remove(connections, i)
+            table.remove(connections, i) -- remove both server and incoming!!!
+            print('removed')
+            break
+          end
+        end
+        for i, connection in ipairs(connections) do
+          if connections_coroutines[connection] == co then
+            connections_coroutines[connection] = nil
+            table.remove(connections, i) -- remove both server and incoming!!!
+            print('removed 2')
             break
           end
         end
--- proxy.lua
+++ proxy.lua
@@ -3,8 +3,8 @@ require 'luarocks.require' -- http://www.luarocks.org/
 require 'async'
 require 'travian'
 
-local function handler(sock_in, co)
-  local url, err = async.receive('', sock_in, '*l')
+local function handler(client, co)
+  local url, err = client:receive('*l')
 
   if not url or url == '' then
     print('error: empty input', err)
@@ -19,34 +19,38 @@ local function handler(sock_in, co)
     print('error:unparsable url'..url)
     return nil
   end
-  print('conn: ', url)
-  local sock_out = async.connect(host, port or 80, co)
-  print('conn1: ', url)
+  print('conn: ', url, host, port)
+  local server = async.connect(host, port or 80, co)
 
-  local request = url..'\r\n'
+  local request = url
   repeat
-    local line = async.receive(url, sock_in, '*l')
+    local line = client:receive('*l')
     if not string.find(line, 'Proxy--Connection') then
-      request = request..line..'\r\n'
+      request = request..'\r\n'..line
     end
   until line == ''
   request = request..'Connection: keep-alive\r\n'
   request = request..'\r\n'
-  async.send(url, sock_out, request)
+  print('['..request..']')
+  -- async.send(url, server, request)
+  server:send(request)
   print('requested: ', url)
 
-  local response, err = async.receive(url, sock_out, '*a')
+  -- local response, err = async.receive(url, server, '*a')
+  local response, err = server:receive('*a')
   if response then
     print('response received: ', url, #response)
   else
     print('response err for', url, err)
-    sock_in.close()
+    client.close()
     return
   end
 
   response = travian.filter(url, 'mimetype', response)
 
-  async.send(url, sock_in, response)
+  client:send(response)
+  -- client:close()
+  -- server:close()
   print('done: ', url)
 end
 
