d6331a575be13502b09e4247b2f0835bf312b10c
Phil Pirj <pirj@mail.ru>
2010-06-08 03:07:41 +0400

    analyzing server response headers, will need to un-gzip body

--- proxy.lua
+++ proxy.lua
@@ -1,6 +1,7 @@
 require 'luarocks.require' -- http://www.luarocks.org/
 require 'async'
 require 'travian'
+require 'util'
 
 local function handler(browser)
   local url, err = async.receive(browser, '*l')
@@ -15,6 +16,7 @@ local function handler(browser)
   local request = {url, 'Connection: close'}
   repeat
     local line = async.receive(browser, '*l')
+    -- print('req line', line)
     if not string.find(line, 'Proxy--Connection') then
       table.insert(request, line)
       if string.find(line, 'Content--Length') then
@@ -32,12 +34,34 @@ local function handler(browser)
 
   async.send(srv, table.concat(request, '\r\n'))
 
-  local data, err, left = async.receive(srv, '*a')  
+  local mimetype, encoding
+  local response_headers = {}
+  repeat
+    local line = async.receive(srv, '*l')
+    -- print('resp line', line)
+
+    table.insert(response_headers, line)
+    if string.find(line, 'Content--Type') then
+      mimetype = string.match(line, 'Content--Type: ([%a/; -=]+)')
+    end
+    if string.find(line, 'Content--Encoding') then
+      encoding = string.match(line, 'Content--Encoding: ([%a/]+)')
+    end
+  until line == ''
+  print('response mimetype', mimetype, 'encoding', encoding)
+  
+  local data, err, left = async.receive(srv, '*a')
   local response = data or left
+  
+  -- !! TODO : DECODE gzip; remove encoding header then
+  -- !! IMPLEMENT AS PROXY to skip unpacking of files from unrelated sites and mimetypes
 
-  response = travian.filter(url, 'mimetype', request, response)
+  response = travian.filter(url, mimetype, request, response)
 
+  table.insert(response_headers, '')
+  async.send(browser, table.concat(response_headers, '\r\n'))
   async.send(browser, response)
+
   -- browser:close()
   -- srv:close()
   print('done: ', url)
